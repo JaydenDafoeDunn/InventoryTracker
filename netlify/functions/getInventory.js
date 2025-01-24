e5b779fe5c24422764af6688bc8ea9d1b5a99f13
JaydenDafoeDunn <96964978+JaydenDafoeDunn@users.noreply.github.com>
2025-01-23 20:11:57 -0700

    Update getInventory.js
    added conole log for debugging

--- netlify/functions/getInventory.js
+++ netlify/functions/getInventory.js
@@ -4,6 +4,7 @@ const path = require('path');
 exports.handler = async (event, context) => {
   try {
     const filePath = path.resolve(__dirname, '../../trackedInventory.json');
+    console.log(`Reading file from: ${filePath}`);
     const data = fs.readFileSync(filePath, 'utf8');
     const inventory = JSON.parse(data);
     return {
@@ -11,6 +12,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(inventory),
     };
   } catch (error) {
+    console.error(`Error fetching inventory: ${error.message}`);
     return {
       statusCode: 500,
       body: `Error fetching inventory: ${error.message}`,
