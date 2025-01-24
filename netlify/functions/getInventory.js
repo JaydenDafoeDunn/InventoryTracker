043f295db40d8db03ef5af9b85ee78713da652bd
JaydenDafoeDunn <96964978+JaydenDafoeDunn@users.noreply.github.com>
2025-01-23 20:39:23 -0700

    Update getInventory.js

--- netlify/functions/getInventory.js
+++ netlify/functions/getInventory.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 exports.handler = async (event, context) => {
   try {
-    const filePath = path.resolve(__dirname, '/trackedInventory.json');
+    const filePath = path.resolve(__dirname, 'trackedInventory.json');
     console.log(`Reading file from: ${filePath}`);
     const data = fs.readFileSync(filePath, 'utf8');
     const inventory = JSON.parse(data);
