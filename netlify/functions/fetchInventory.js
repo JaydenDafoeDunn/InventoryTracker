03fe95d08c031ba77f443d96754e1c9cf1916b6e
JaydenDafoeDunn <96964978+JaydenDafoeDunn@users.noreply.github.com>
2025-01-23 19:07:00 -0700

    Add files via upload
    added fetchInventory

---
+++ netlify/functions/fetchInventory.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const JSONBIN_API_URL = 'https://api.jsonbin.io/v3/b';
+const BIN_ID = process.env.JSONBIN_BIN_ID;
+const SECRET_KEY = process.env.JSONBIN_SECRET_KEY;
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(`${JSONBIN_API_URL}/${BIN_ID}/latest`, {
+      headers: {
+        'X-Master-Key': SECRET_KEY,
+      },
+    });
+    const data = await response.json();
+    if (Array.isArray(data.record) && data.record.every(item => typeof item.name === 'string' && typeof item.status === 'string')) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data.record),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: 'Invalid inventory data',
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `Error fetching inventory from JSONBIN: ${error.message}`,
+    };
+  }
+};
